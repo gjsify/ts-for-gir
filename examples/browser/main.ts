cd8b3b2f0d0c3a3d5bee542dfc0f76b812c006f8
sam <sam.jansen@gmail.com>
2017-06-26 23:09:43 +0100

    More test code. WIP.

--- examples/browser/main.ts
+++ examples/browser/main.ts
@@ -3,4 +3,21 @@ import * as WebKit from '../../out/WebKit'
 
 Gtk.init(null)
 
+let wnd = new Gtk.Window({ title: 'Browser Test', default_width: 400, default_height: 300 })
+let box = new Gtk.Box({ valign: Gtk.Align.FILL, halign: Gtk.Align.FILL })
+let scroll = new Gtk.ScrolledWindow({ valign: Gtk.Align.FILL, halign: Gtk.Align.FILL })
+let webview = new WebKit.WebView({ valign: Gtk.Align.FILL, halign: Gtk.Align.FILL })
+
+webview.load_uri("http://www.google.com")
+scroll.add(webview)
+box.add(scroll)
+wnd.add(box)
+wnd.show_all()
+
+wnd.connect("delete-event", (obj, event) => {
+    Gtk.main_quit()
+    return true
+})
+
+Gtk.main()
 

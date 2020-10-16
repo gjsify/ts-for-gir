72e7ce8f80b38c598a4adc077c0354089fc8de18
Tony Houghton <h@realh.co.uk>
2020-10-16 17:36:40 +0100

    Added callback field to GirVariable

--- src/types/gir-function.ts
+++ src/types/gir-function.ts
@@ -1,18 +1,1 @@
-import { TsForGjsExtended } from './ts-for-gjs-extended'
-import { GirVariable } from './gir-variable'
-import { GirDoc } from './gir-doc'
-import { GirParameter } from './gir-parameter'
-
-export interface GirFunction extends TsForGjsExtended {
-    $: {
-        name: string
-        version?: string
-        'c-identifier'?: string
-        introspectable?: string
-        'moved-to'?: string
-        'shadowed-by'?: string
-    }
-    doc?: GirDoc[]
-    parameters?: GirParameter[]
-    'return-value'?: GirVariable[]
-}
+export { GirFunction } from './gir-variable'
--- src/types/gir-variable.ts
+++ src/types/gir-variable.ts
@@ -2,6 +2,10 @@ import { TsForGjsExtended } from './ts-for-gjs-extended'
 import { GirDoc } from './gir-doc'
 import { GirType } from './gir-type'
 import { GirArray } from './gir-array'
+import { GirParameter } from './gir-parameter'
+
+// Declaring these two together is the easiest way to prevent circular
+// imports
 
 export interface GirVariable extends TsForGjsExtended {
     $: {
@@ -21,4 +25,19 @@ export interface GirVariable extends TsForGjsExtended {
     doc?: GirDoc[]
     type?: GirType[]
     array?: GirArray[]
+    callback?: GirFunction
+}
+
+export interface GirFunction extends TsForGjsExtended {
+    $: {
+        name: string
+        version?: string
+        'c-identifier'?: string
+        introspectable?: string
+        'moved-to'?: string
+        'shadowed-by'?: string
+    }
+    doc?: GirDoc[]
+    parameters?: GirParameter[]
+    'return-value'?: GirVariable[]
 }

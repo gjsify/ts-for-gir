73cc300ef77f330e37e6cb198c3da34eec97d7fd
Pascal Garber <pascal@jumplink.eu>
2022-01-06 21:09:53 +0100

    Revert "Merge"
    This reverts commit 5461104aa0949681f1f3fe4dff3d36454da451e9.

--- .ts-for-gir.all.rc.js
+++ .ts-for-gir.all.rc.js
@@ -6,5 +6,5 @@ module.exports = {
     ignore: [
         'Colorhug-1.0', // Duplicate of ColorHug-1.0
         'GUPnP-DLNA-1.0', // Same namespace as GUPnP-1.0.gir, is this a bug or should we merge the type definitions?
-    ]
+    ],
 }

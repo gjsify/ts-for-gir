09e7fda3b9140639650ab2e30068692ad858c5b1
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2022-05-26 02:27:31 -0300

    Tell tsc to expect error on gi ESM imports

--- templates/Gjs/module.js
+++ templates/Gjs/module.js
@@ -1,14 +1,15 @@
 <% if(moduleType === 'esm'){ %>
+// @ts-expect-error
 import <%= name %> from 'gi://<%= name %>?version=<%= version %>';
 export { <%= name %> };
 export default <%= name %>;
-<% } else { %>  
+<% } else { %>
 imports.gi.versions.<%= name %> = '<%= version %>'
 const <%= name %> = imports.gi.<%= name %>;
   <% if(useNamespace){ %>
 module.exports = { <%= name %> };
 exports.default = <%= name %>;
-  <% } else { %>  
+  <% } else { %>
 module.exports = <%= name %>;
   <% } %>
 <% } %>

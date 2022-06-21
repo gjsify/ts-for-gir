a75fc305dc42d75e59be67ae9fb71b4e435962f9
Pascal Garber <pascal@artandcode.studio>
2022-06-21 18:23:52 +0200

    Tests: Re-enable typescript semantic checks for the tests

--- .ts-for-gir.gcalc.rc.js
+++ .ts-for-gir.gcalc.rc.js
@@ -2,6 +2,7 @@ export default {
     modules: [/*'GCalc-1',*/ 'GCalc-2'],
     girDirectories: ['./vala-girs/gir-1.0'],
     pretty: false,
+    noCheck: false,
     ignore: [],
     ignoreVersionConflicts: true
 }

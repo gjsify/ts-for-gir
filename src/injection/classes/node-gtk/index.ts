5de675de87aee72e47c4569c929c329978643bb0
Pascal Garber <pascal@artandcode.studio>
2022-07-04 11:53:31 +0200

    Removed type patches

--- src/injection/classes/node-gtk/index.ts
+++ src/injection/classes/node-gtk/index.ts
@@ -6,7 +6,7 @@ import { classesGObject20Node } from './GObject-2.0.js'
 import { classesGraphene10Node } from './Graphene-1.0.js'
 import { classesGtk30Node } from './Gtk-3.0.js'
 import { classesGtk40Node } from './Gtk-4.0.js'
-import { classesPango10Node } from './Pango-1.0'
+import { classesPango10Node } from './Pango-1.0.js'
 
 export const classesNode: InjectionClass[] = [
     ...classesGdk40Node,

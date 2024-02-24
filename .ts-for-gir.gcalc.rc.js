fa810042dd6421f8e627452abffe9b262126572c
Pascal Garber <pascal@artandcode.studio>
2024-02-24 07:23:37 +0100

    Do not use removed CLI options

--- .ts-for-gir.gcalc.rc.js
+++ .ts-for-gir.gcalc.rc.js
@@ -1,5 +1,4 @@
 export default {
-    environments: ['gjs'],
     modules: [/*'GCalc-1',*/ 'GCalc-2'],
     girDirectories: ['./vala-girs/gir-1.0'],
     ignore: [],
--- .ts-for-gir.timezonemap1.rc.js
+++ .ts-for-gir.timezonemap1.rc.js
@@ -1,5 +1,4 @@
 export default {
-    environments: ['gjs'],
     modules: ['TimezoneMap-1.0'],
     girDirectories: ['./vala-girs/gir-1.0'],
     ignore: [],

c8676cda08873a84d4ddeb5c4128a3de9125f49f
Pascal Garber <pascal@artandcode.studio>
2024-02-24 07:23:37 +0100

    Do not use removed CLI options

--- .ts-for-gir.gtk3.rc.js
+++ .ts-for-gir.gtk3.rc.js
@@ -1,5 +1,4 @@
 export default {
-    environments: ['gjs'],
     modules: ['Gtk-3.0'],
     girDirectories: ['./vala-girs/gir-1.0'],
     ignore: [],
--- .ts-for-gir.vte4.rc.js
+++ .ts-for-gir.vte4.rc.js
@@ -1,5 +1,4 @@
 export default {
-    environments: ['gjs'],
     modules: ['Vte-4*'],
     girDirectories: ['./vala-girs/gir-1.0'],
     ignore: [],

1995beb604971edd9cedefd224b8d73deec1446c
Pascal Garber <pascal@artandcode.studio>
2024-02-25 01:44:35 +0100

    Cleanup type generation configs

--- .ts-for-gir.gio.rc.js
+++
@@ -1,8 +0,0 @@
-export default {
-    modules: ['Gio-2.0'],
-    girDirectories: ['./vala-girs/gir-1.0'],
-    ignore: [],
-    promisify: true,
-    verbose: true,
-    outdir: './types',
-}
--- .ts-for-gir.vda1.rc.js
+++
@@ -1,5 +0,0 @@
-export default {
-    modules: ['Vda-1'],
-    girDirectories: ['./vala-girs/gir-1.0'],
-    ignore: [],
-}

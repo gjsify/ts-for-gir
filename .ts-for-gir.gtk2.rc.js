b538df5667d10dc0582c35d245613c9f40c8b766
Pascal Garber <pascal@artandcode.studio>
2024-02-25 01:44:35 +0100

    Cleanup type generation configs

--- .ts-for-gir.gtk2.rc.js
+++
@@ -1,5 +0,0 @@
-export default {
-    modules: ['Gtk-2.0'],
-    girDirectories: ['./vala-girs/gir-1.0'],
-    ignore: [],
-}
--- .ts-for-gir.modemmanager1.rc.js
+++
@@ -1,5 +0,0 @@
-export default {
-    modules: ['ModemManager-1.0'],
-    girDirectories: ['./vala-girs/gir-1.0'],
-    ignore: [],
-}

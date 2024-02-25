317e95509766c531079ecab6d677cca613b5bc29
Pascal Garber <pascal@artandcode.studio>
2024-02-25 01:44:35 +0100

    Cleanup type generation configs

--- .ts-for-gir.all.rc.js
+++ .ts-for-gir.all.rc.js
@@ -6,5 +6,8 @@ export default {
     ignore: [
         'Colorhug-1.0', // Duplicate of ColorHug-1.0
         'GUPnP-DLNA-1.0', // Same namespace as GUPnP-1.0.gir, is this a bug or should we merge the type definitions?
-    ]
+        'GstBase-1.0', // Unable to resolve type: BaseSink from GstBase in ClutterGst 1.0
+        'ClutterGst-1.0', // Depends on GstBase-1.0
+        'GstAudio-0.10', // Depends on GstBase-1.0
+    ],
 }
--- .ts-for-gir.timezonemap1.rc.js
+++
@@ -1,5 +0,0 @@
-export default {
-    modules: ['TimezoneMap-1.0'],
-    girDirectories: ['./vala-girs/gir-1.0'],
-    ignore: [],
-}

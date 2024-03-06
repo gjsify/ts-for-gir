f2a7bb4c00c2e0fc5320c54f2e4772ecaffffd21
Evan Welsh <ewlsh@gnome.org>
2024-03-05 22:57:35 -0800

    Up ts-for-gir version and point types to 'next'

--- .ts-for-gir.gtk4.rc.js
+++ .ts-for-gir.gtk4.rc.js
@@ -1,6 +1,6 @@
 export default {
     environments: ['gjs'],
-    modules: ['Gtk-4.0'],
+    modules: ['Gtk-4.0', 'Adw-1.0'],
     girDirectories: ['./vala-girs/gir-1.0'],
     ignore: [],
     ignoreVersionConflicts: true,

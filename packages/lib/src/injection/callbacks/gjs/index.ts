9036566a5ee7c811d158394834dd58a82e5ec927
Evan Welsh <contact@evanwelsh.com>
2024-03-11 01:17:45 -0700

    lib: Merge injection sources from gi.ts and ts-for-gir

--- packages/lib/src/injection/callbacks/gjs/index.ts
+++
@@ -1,5 +0,0 @@
-import type { InjectionCallback } from '../../../types/index.js'
-
-import { callbacksGio20Gjs } from './Gio-2.0.js'
-
-export const callbacksGjs: InjectionCallback[] = [...callbacksGio20Gjs]

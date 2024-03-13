04a2ee3e53a880276ad0343473a966f7072ab1ab
Evan Welsh <contact@evanwelsh.com>
2024-03-13 00:58:24 -0700

    ci: Build local types in a separate project

--- .ts-for-gir.all.rc.js
+++ .ts-for-gir.all.rc.js
@@ -1,8 +1,10 @@
 export default {
+    outdir: './local/@girs/',
     modules: ['*'],
     girDirectories: ['./vala-girs/gir-1.0', './girs'],
     ignoreVersionConflicts: true,
     promisify: true,
+    packageYarn: true,
     ignore: [
         'Colorhug-1.0', // Duplicate of ColorHug-1.0
         'GUPnP-DLNA-1.0', // Same namespace as GUPnP-1.0.gir, is this a bug or should we merge the type definitions?

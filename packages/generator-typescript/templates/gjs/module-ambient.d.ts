39a457f3f90bf0e56d9cc0f483319c45f3cbbf11
Evan Welsh <ewlsh@gnome.org>
2024-03-13 01:33:36 -0700

    typescript: Import and export using the namespace name for better intellisense

--- packages/generator-typescript/templates/gjs/module-ambient.d.ts
+++ packages/generator-typescript/templates/gjs/module-ambient.d.ts
@@ -5,14 +5,14 @@ _%>
 <%_ const pkg = dep.get(girModule.namespace, girModule.version) _%>
 <%_ let moduleImportStr = ""; _%>
 <%_ if(noNamespace){ _%>
-    <%_ moduleImportStr = `import * as ${girModule.importNamespace} from '${pkg.importPath}'`; _%>
+    <%_ moduleImportStr = `import * as ${girModule.namespace} from '${pkg.importPath}'`; _%>
 <%_ } else { _%>
-    <%_ moduleImportStr = `import ${girModule.importNamespace} from '${pkg.importPath}'`; _%>
+    <%_ moduleImportStr = `import ${girModule.namespace} from '${pkg.importPath}'`; _%>
 <%_ } _%>
 
 declare module 'gi://<%= name %>?version=<%= version %>' {
     <%- moduleImportStr %>;
-    export default <%- girModule.importNamespace -%>;
+    export default <%- girModule.namespace -%>;
 }
 
 <%# // Generate ambient module declarations Without version number if there are no conflicts or the target is an NPM package _%>

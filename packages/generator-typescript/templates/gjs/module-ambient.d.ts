e2560ed91881d83a9da5f8ffc43b23e447c94ee6
Pascal Garber <pascal@artandcode.studio>
2024-04-04 00:19:55 -0700

    Examples: Fix some global type settings

--- packages/generator-typescript/templates/gjs/module-ambient.d.ts
+++ packages/generator-typescript/templates/gjs/module-ambient.d.ts
@@ -15,7 +15,8 @@ declare module 'gi://<%= name %>?version=<%= version %>' {
     export default <%- girModule.namespace -%>;
 }
 
-<%# // Generate ambient module declarations Without version number if there are no conflicts or the target is an NPM package _%>
+<%# // Generate ambient module declarations Without version number if this is the latest version _%>
+<%_ if (dep.isLatestVersion(pkg.namespace, pkg.version)) { _%>
 declare module 'gi://<%= name %>' {
     import <%- girModule.importNamespace -%> from 'gi://<%= name %>?version=<%= version %>';
     export default <%- girModule.importNamespace -%>;

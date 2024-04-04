466f589a07d4a542951576a33edd55f22e553b13
Pascal Garber <pascal@artandcode.studio>
2024-04-04 19:49:19 +0200

    Only build packages for gir files included in the repository

--- .ts-for-gir.all.rc.js
+++ .ts-for-gir.all.rc.js
@@ -1,6 +1,49 @@
 export default {
     modules: ['*'],
-    girDirectories: ['./vala-girs/gir-1.0', './girs'],
+    girDirectories: [
+
+        // General gir files in this repository
+        './vala-girs/gir-1.0',
+        './girs',
+
+        // General gir files installed on the system
+        "/usr/local/share/gir-1.0",
+        "/usr/share/gir-1.0",
+
+        // GNOME Shell gir file (package: gnome-shell-common / gnome-shell)
+        '/usr/share/gnome-shell',
+        '/usr/share/gnome-shell/gir-1.0',
+
+        // GNOME Shell gir file dependencies on Fedora Workstation 36 (package: mutter)
+        '/usr/lib64/mutter-10',
+
+        // GNOME Shell gir file dependencies on Fedora Workstation 37 (package: mutter)
+        '/usr/lib64/mutter-11',
+
+        // GNOME Shell gir file dependencies on Fedora Workstation 38 (package: mutter)
+        '/usr/lib64/mutter-12',
+
+        // GNOME Shell gir file dependencies on Fedora Workstation 39 (package: mutter)
+        '/usr/lib64/mutter-13',
+
+        // Wait for release...
+        '/usr/lib64/mutter-14',
+
+        // GNOME Shell gir file dependencies on Ubuntu 22.04 (package: libmutter-10-dev)
+        '/usr/lib/x86_64-linux-gnu/mutter-10',
+
+        // GNOME Shell gir file dependencies on Ubuntu 22.10 (package: libmutter-11-dev)
+        '/usr/lib/x86_64-linux-gnu/mutter-11',
+
+        // GNOME Shell gir file dependencies on Ubuntu 23.04 (package: libmutter-12-dev)
+        '/usr/lib/x86_64-linux-gnu/mutter-12',
+
+        // GNOME Shell gir file dependencies on Ubuntu 23.10 (package: libmutter-13-dev)
+        '/usr/lib/x86_64-linux-gnu/mutter-13',
+
+        // GNOME Shell gir file dependencies on Ubuntu package: libmutter-14-dev) and GNOME OS
+        '/usr/lib/x86_64-linux-gnu/mutter-14', 
+    ],
     ignoreVersionConflicts: true,
     promisify: true,
     packageYarn: true,

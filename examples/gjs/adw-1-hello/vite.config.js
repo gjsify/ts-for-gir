707d90e9f80ebdfeee56f2b22635cf97d02cc4ea
Pascal Garber <pascal@artandcode.studio>
2024-07-09 10:55:00 +0200

    Moved test package to tests dir and update esbuild configs for latest SpiderMonkey version used by GJS

--- examples/gjs/adw-1-hello/vite.config.js
+++ examples/gjs/adw-1-hello/vite.config.js
@@ -16,11 +16,13 @@ export default defineConfig({
         babel({
           babelHelpers: 'bundled',
           targets: {
-            firefox: 91
+            // firefox: 91
             // firefox: 60, // Since GJS 1.53.90
             // firefox: 68, // Since GJS 1.63.90
             // firefox: 78, // Since GJS 1.65.90
             // firefox: 91, // Since GJS 1.71.1
+            // firefox: 102, // Since GJS 1.73.2
+            firefox: 115, // Since GJS 1.77.2
           }
         })
       ]

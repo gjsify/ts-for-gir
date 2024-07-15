57c1121f77dcea1f400700be6424ea27236dca18
Pascal Garber <pascal@artandcode.studio>
2024-07-15 14:08:31 +0200

    Prepare the test for locally generated types

---
+++ tests/types-package/.ts-for-gir.gtk-2-3-4.rc.js
@@ -0,0 +1,8 @@
+export default {
+    modules: ['Gtk-2.0', 'Gtk-3.0', 'Gtk-4.0',],
+    girDirectories: [
+        '../../girs',
+    ],
+    ignoreVersionConflicts: true,
+    onlyVersionPrefix: true,
+}

e0ee9ab4cbf47591fb171032ea40cab0f9589259
Pascal Garber <pascal@artandcode.studio>
2025-06-25 14:16:12 +0200

    chore: Update import statements and log domain usage in main.ts
    - Changed import statement to use 'console' instead of 'node:console' for GJS compatibility.
    - Simplified log domain setting and console log calls by using single quotes and removing unnecessary semicolons.

--- examples/console-tsc/main.ts
+++ examples/console-tsc/main.ts
@@ -1,15 +1,16 @@
 // Example from issue, see https://github.com/gjsify/ts-for-gir/issues/164
 
-import { DEFAULT_LOG_DOMAIN, getConsoleLogDomain, setConsoleLogDomain } from "node:console";
+// biome-ignore lint/style/useNodejsImportProtocol: This is GJS not Node.js
+import { DEFAULT_LOG_DOMAIN, getConsoleLogDomain, setConsoleLogDomain } from 'console'
 
 // Setting the log domain
-setConsoleLogDomain("my.app.id");
+setConsoleLogDomain('my.app.id')
 
 // expected output: my.app.id-Message: 12:21:17.899: cool
-console.log("cool");
+console.log('cool')
 
 // Checking and resetting the log domain
-if (getConsoleLogDomain() !== DEFAULT_LOG_DOMAIN) setConsoleLogDomain(DEFAULT_LOG_DOMAIN);
+if (getConsoleLogDomain() !== DEFAULT_LOG_DOMAIN) setConsoleLogDomain(DEFAULT_LOG_DOMAIN)
 
 // expected output: Gjs-Console-Message: 12:21:17.899: cool
-console.log("cool");
+console.log('cool')
